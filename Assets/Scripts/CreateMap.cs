3e1cc1910b1094ea31b9b6d28c89d6422c246631
BobOzgun <akkoyun.ozgun@gmail.com>
2020-01-20 17:02:21 +0300

    1/20/2020 Ende of day

--- Assets/Scripts/CreateMap.cs
+++ Assets/Scripts/CreateMap.cs
@@ -16,16 +16,20 @@ public class CreateMap : MonoBehaviour
     public void MapCreate(int stepSize)
     {
         ri = FindObjectOfType<RoadInstatiate>();
-        for (int i = -24; i < 20; i+= stepSize)
+        for (int i = -20; i < 20; i+= stepSize)
         {
-            for (int j = -24; j < 20; j+= stepSize)
+            for (int j = -20; j < 20; j+= stepSize)
             {
                 setPos = new Vector3(i, 0.07f, j);
                 Debug.Log(ri.side.FindIndex(element => element.pos == setPos));
                 //Debug.Log(ri.squareSize);
                 if ( ri.side.FindIndex(element => element.pos==setPos)<0 )
                 {
-                    Instantiate(mapObjects[0], new Vector3(i, 0, j), mapObjects[0].transform.rotation);
+                    Random.seed = System.DateTime.Now.Millisecond;
+                    //System.Random rnd = new System.Random();
+                    var olasilik =(int) Mathf.Round(Random.Range(0, mapObjects.Length));
+                    //var olasilik = rnd.Next(0,mapObjects.Length);
+                    Instantiate(mapObjects[olasilik], new Vector3(i, 0, j), mapObjects[olasilik].transform.rotation);
                 }
                
             }
